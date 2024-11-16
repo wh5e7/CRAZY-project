4b68f41b74955e2c70aed012879b4a1ebd5e0bc3
wh5e7 <136388484+wh5e7@users.noreply.github.com>
2024-11-16 15:09:08 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+var password = "descend";
+
+function passcheck() {
+
+if(document.getElementById("password1").value != password) {
+    return false;
+}
+
+if(document.getElementById("password1").value == password) {
+}
+}
